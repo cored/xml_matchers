9f5734bc9fe0970b4655fb549fdc20691e612c45
Rafael George <george.rafael@gmail.com>
2018-07-15 18:40:59 -0400

    Add XmlMatchers::Assertion to properly separate concerns
    Now there's an specific component that handles validation

--- lib/xml_matchers.rb
+++ lib/xml_matchers.rb
@@ -3,19 +3,33 @@ require "xml_matchers/version"
 
 module XmlMatchers
   InvalidSchemaError = Class.new(StandardError)
+
+  class Assertion
+    def self.for(schema_content)
+      new(xml_schema: Nokogiri::XML::Schema(schema_content.to_s))
+    rescue Nokogiri::XML::SyntaxError
+      raise InvalidSchemaError
+    end
+
+    def initialize(xml_schema:)
+      @xml_schema = xml_schema
+    end
+
+    def valid?(xml_content)
+      xml_document = Nokogiri::XML(xml_content)
+      xml_schema.valid?(xml_documet)
+    end
+
+    private
+
+    attr_reader :xml_schema
+  end
 end
 
 RSpec::Matchers.define :match_xml_schema do |schema_content|
-  def self.build_schema_for(schema_content)
-    Nokogiri::XML::Schema(schema_content.to_s)
-  rescue Nokogiri::XML::SyntaxError
-    raise XmlMatchers::InvalidSchemaError
-  end
-
-  schema = build_schema_for(schema_content)
+  assertion = XmlMatchers::Assertion.for(schema_content)
 
   match do |xml|
-    document = Nokogiri::XML(xml)
-    schema.valid?(document)
+    assertion.valid?(xml)
   end
 end
